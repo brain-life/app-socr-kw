26f939b0b0a3eac2cd37bb3f461b58293baa6aa5
Soichi Hayashi <hayashis@iu.edu>
2018-07-17 17:53:08 +0000

    updated to generate dm.txt using _input.meta

--- kw.sh
+++ kw.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+#http://wiki.stat.ucla.edu/socr/index.php/SOCR_EduMaterials_AnalysesCommandLineVolumeMultipleRegression
 java -cp /socr/jars/SOCR_core.jar:/socr/jars/SOCR_plugin.jar \
     edu.ucla.stat.SOCR.analyses.command.volume.VolumeMultipleRegression \
     -dm dm.txt -h -regressors CDR,MMSE -dim 220 220 220 -intercept interceptConstant
